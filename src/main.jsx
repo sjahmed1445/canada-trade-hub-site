84bc0f55219c15299967212fae8569bfa5abf7ae
Jawad Syed <54323440+sjahmed1445@users.noreply.github.com>
2025-08-10 18:22:27 -0400

    Add all project files

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import CanadaTradeHub from "./CanadaTradeHub_SPA.jsx";
+
+createRoot(document.getElementById("root")).render(<CanadaTradeHub />);
